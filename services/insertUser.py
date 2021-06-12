8fd74b4f5c607685a6ffc90b6d2f35a271dff348
alayug <layugaliana@gmail.com>
2021-06-12 15:41:56 -0500

    added insertUser function to insert user data to csv

--- services/insertUser.py
+++ services/insertUser.py
@@ -1,3 +1,12 @@
-# Takes in list with following values - first_name,last_name,email_address,phone_number,task_id
+import csv
 
-# Insert new row in users.csv with list
+
+# Takes in list with following values - first_name,last_name,email_address,phone_number,task_id
+def insert_task(firstname, lastName, emailAddress, phoneNumber, taskID):
+
+    fields=[firstname, lastName, emailAddress, phoneNumber, taskID]
+
+    # Insert new row in users.csv with list
+    with open(r'C:\bench\user-tester-manager\database\users.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(fields)

c6b078435bf502f67a631a9e812e0a42941f9a33
alayug <layugaliana@gmail.com>
2021-06-12 14:54:54 -0500

    added insertTask functionality

--- services/insertTask.py
+++ services/insertTask.py
@@ -1,3 +1,17 @@
 # Takes in task name
 
 # Insert new row in tasks.csv with task name
+import csv
+
+
+
+def insertTask(taskID, taskName):
+
+    fields=[taskID,taskName]
+
+    with open(r'C:\bench\user-tester-manager\database\tasks.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(fields)
+
+
+insertTask(1,"test")

b1e63f8d0e7e27bcbf4647fbf485d9d857aeb404
alayug <layugaliana@gmail.com>
2021-06-13 03:24:38 -0500

    add getUserDetails service, cleaned insertTask and index file

--- services/getUserDetails.py
+++ services/getUserDetails.py
@@ -1,1 +1,16 @@
-# Takes in user ID to retrieve user info
+import csv
+
+# Takes in task name
+def get_user_details():
+    # Create list to store the csv data
+    userDetailsList = []
+
+    # opens csv file
+    with open(r'C:\bench\user-tester-manager\database\users.csv', 'r') as f:
+        # csv reader will each line of data in the csv file
+        reader = csv.reader(f)
+        # for each row in the csv file, append to userDetailsList
+        for row in reader:
+            userDetailsList.append(row)
+  
+    return userDetailsList
--- services/insertTask.py
+++ services/insertTask.py
@@ -1,14 +1,11 @@
-# Takes in task name
 
-# Insert new row in tasks.csv with task name
 import csv
 
-
-
+# Takes in task name
 def insert_task(taskID, taskName):
-
     fields=[taskID,taskName]
 
+    # Insert new row in tasks.csv with task name
     with open(r'C:\bench\user-tester-manager\database\tasks.csv', 'a') as f:
         writer = csv.writer(f)
         writer.writerow(fields)

187727c635490e2358741273e3b0d02a469bb84e
alayug <layugaliana@gmail.com>
2021-06-27 12:07:42 -0500

    added getTaskDetailNames service call to retrieve list of task names

--- services/getTaskDetails.py
+++ services/getTaskDetails.py
@@ -13,3 +13,17 @@ def get_task_details():
             taskList.append(row)
   
     return taskList
+
+def get_task_detail_names():
+    # Create list to store the csv data
+    taskListNames = []
+
+    # opens csv file, using 'r' for read
+    with open(r'database\tasks.csv', 'r') as f:
+        # csv reader will each line of data in the csv file
+        reader = csv.reader(f)
+        # for each row in the csv file, append to taskList
+        for row in reader:
+            taskListNames.append(row[1])
+  
+    return taskListNames

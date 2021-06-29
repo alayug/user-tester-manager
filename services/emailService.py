d2e64fe318f818e33130f28c5a113650dd41fe3f
alayug <layugaliana@gmail.com>
2021-06-28 22:47:05 -0500

    implemented email template to integrate name and task name to email

--- services/emailService.py
+++ services/emailService.py
@@ -1,14 +1,14 @@
 import smtplib
 from email.message import EmailMessage
 
-def send_email(email, password):
+def send_email(email, password, firstName, taskName):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
     sender_email = "alianapythonclass@gmail.com" 
 
     msg = EmailMessage()
     # Set the body/message of your email
-    msg.set_content('Hello! This is a reminder to do your task! :)')
+    msg.set_content('Hello %s,\n\nThis is a reminder to complete the following task:\n\nTask - %s' % (firstName, taskName))
 
     msg['Subject'] = 'Python User Tester Manager'
     msg['From'] = sender_email

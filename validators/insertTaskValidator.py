c53460b06f7220ccf9d9e1b7cd34b5aef8c9cc8b
alayug <layugaliana@gmail.com>
2021-07-22 00:56:25 -0500

    add whitespace validation in taskNameValidator

--- validators/insertTaskValidator.py
+++ validators/insertTaskValidator.py
@@ -1,6 +1,6 @@
 def taskNameValidator(taskName):
     message = ""
-    if taskName == "":
+    if taskName == "" or taskName.isspace() == True:
         message = "Task Name can't be empty!"
     elif len(taskName) > 50:
         message = "Task Name can't be more than 50 characters"
